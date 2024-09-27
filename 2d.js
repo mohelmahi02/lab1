e2d550cd7b4f34ab49674be27a7b1225bf6221b6
Mohammad Elmahi <G00407950@atu.ie>
2024-09-27 11:01:51 +0000

    2d finished

--- 2d.js
+++ 2d.js
@@ -1,8 +1,13 @@
 //Returning array values under 70 by 2
 const ages = [25,31,42,77];
 
+
 let d = ages.map((item)=>{
+    if (item < 70)
     return item * 2;
+    else
+    
+    return item;
 })
 //Values multiplied by 2 
 console.log(d);
